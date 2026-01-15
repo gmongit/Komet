d6c9d23d675f02b87c5c5904021c46fc1d585ba3
nsextro-code <nsextro@gmail.com>
2026-01-15 23:19:09 +0100

    Add files via upload

--- simpleG.m
+++ simpleG.m
@@ -1,26 +1,17 @@
 function g = simpleG(r)
-%SIMPLEG Gravitationsfeld (Beschleunigung) nur von der Sonne.
-%   g = simpleG(r)
-%   r ... Position (2x1 oder 1x2) relativ zur Sonne [m]
-%   g ... Beschleunigung am Punkt r durch Sonne [m/s^2]
-%
-%   Richtung: immer ZUM Massezentrum => Minuszeichen!
 
-    % Konstanten (SI)
-    G      = 6.67430e-11;       % [m^3/(kg*s^2)]
-    M_sun  = 1.98847e30;        % [kg]
+    G      = 6.67430e-11;   %Gravitationskonstante in m³/(kg·s²)
+    M_sun  = 1.98847e30;    %Masse der Sonne in kg    
 
-    % Sicherstellen: Spaltenvektor
-    r = r(:);
+   
+    r = r(:); %Spaltenvektor
 
     % Abstand
-    d = norm(r);
+    d = norm(r); %Abstand vom Punkt r zur Sonne(a^2 + b^2)
 
-    % Schutz gegen Division durch 0
     if d == 0
         error('simpleG: r darf nicht [0;0] sein (Abstand = 0).');
     end
 
-    % Gravitationsbeschleunigung: g = -G*M/|r|^3 * r
-    g = -G * M_sun / d^3 * r;
+    g = -G * M_sun / d^3 * r; %je größer d desto geringer g
 end
